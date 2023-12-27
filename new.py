6fb15dbd8ab87e32eabd29ff1f93d4f782235007
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2023-12-27 17:47:18 +0100

    hmmm

--- new.py
+++ new.py
@@ -12,7 +12,7 @@ v=[1,2,3,4,5,6,7,8]
 h=[47,67,73,75,20,30,40,47]
 print(len(v))
 print(len(h))
-plt.plot(v,h, color='red',linestyle='dashed')
+plt.plot(v,h, color='red',linestyle='dashed', markersize=6)
 plt.xlabel('day')
 plt.ylabel('Temperature')
 plt.title('Tempreture accross Asia')
