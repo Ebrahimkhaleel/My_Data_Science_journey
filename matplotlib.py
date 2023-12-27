94d8a11ae027c27c24b4b8d11a14eaeb007328ad
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2023-12-27 08:08:33 +0100

    my name

--- matplotlib.py
+++ matplotlib.py
@@ -3,8 +3,8 @@ import pandas as ph as a
 import numpy as hp
 import seaborn as sns
 import requests as rq
-<<<<<<< HEAD
 import math as my
-=======
-import math as my
->>>>>>> new
+
+print("my name is kahleel")
+
+
