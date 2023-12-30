e936d9bffee21f740e0324deb6e91c7680831f03
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2023-12-30 13:33:42 +0100

    matplotlib subplot

--- new.py
+++ new.py
@@ -1,4 +1,4 @@
-import pandas as pd 
+"""import pandas as pd 
 import matplotlib.pyplot as plt
 
 
@@ -18,3 +18,31 @@ plt.ylabel('Temperature')
 plt.title('Tempreture accross Asia')
 plt.show()
 
+days = range(1,8) 
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.linspace(0,5,11)
+y=x**2
+plt.subplot(1,2,1)
+plt.plot(x,y,'y')
+
+plt.subplot(1,2,2)
+plt.plot(y,x,'g')
+plt.show()"""
+import matplotlib.pyplot as plt
+import numpy as np
+
+x= np.linspace(0,5,11)
+y=x**2
+
+fig= plt.figure()
+ 
+axes1=fig.add_axes([0.1,0.1,0.8,0.8])
+axes2=fig.add_axes([0.2,0.5,0.4,0.3])
+axes1.plot(y,x)
+axes1.set_title('large')
+axes2.plot(x,y)
+axes2.set_title('small')
+plt.show()
