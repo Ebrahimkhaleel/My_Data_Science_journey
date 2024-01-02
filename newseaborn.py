c23d612e1d1783554b1d49ce23ee5188e7acc206
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2024-01-02 23:39:57 +0100

    plots are making sense

--- newseaborn.py
+++ newseaborn.py
@@ -1,1 +1,12 @@
 import seaborn as sns
+import matplotlib.pyplot as plt
+
+tips=sns.load_dataset('tips')
+print(tips.head())
+#distplot
+sns.displot(tips['total_bill'], kde=True, bins=30)
+#joint plot
+sns.jointplot(x='total_bill', y='tip',data=tips,kind='reg')
+#pairplot
+sns.pairplot(tips,hue='sex', palette='coolwarm')
+plt.show()
