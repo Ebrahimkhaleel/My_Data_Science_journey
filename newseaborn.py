2ab0334f5ccdba8b6428873d6751e83cc5ad7125
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2024-01-04 11:10:06 +0100

    on stripplot split deprecated to dodge

--- newseaborn.py
+++ newseaborn.py
@@ -1,4 +1,4 @@
-import seaborn as sns
+"""import seaborn as sns
 import matplotlib.pyplot as plt
 
 tips=sns.load_dataset('tips')
@@ -9,4 +9,21 @@ sns.displot(tips['total_bill'], kde=True, bins=30)
 sns.jointplot(x='total_bill', y='tip',data=tips,kind='reg')
 #pairplot
 sns.pairplot(tips,hue='sex', palette='coolwarm')
+plt.show()
+#categorical plot
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as  np
+tips=sns.load_dataset('tips')
+sns.barplot(x='sex',y='total_bill',data=tips, estimator=np.std)
+sns.countplot(x='sex',data=tips)
+sns.boxplot(x='day',y='total_bill',data=tips)
+#VIOLINPLOT
+sns.violinplot(x='day',y='total_bill',data=tips,hue='sex',split=True)
+plt.show()"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+tips=sns.load_dataset('tips')
+#STRIPPLOT
+sns.stripplot(x='day',y='total_bill',data=tips,hue='sex',jitter=True,dodge=True)
 plt.show()
