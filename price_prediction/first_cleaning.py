165f16168f70a08daa72bce29447a9901e2f057b
Ebrahimkhaleel <ibrahimkhaleel0123@gmail.com>
2024-01-08 12:04:40 +0100

    bypasssed as my data is refusing to create a new column

--- price_prediction/first_cleaning.py
+++ price_prediction/first_cleaning.py
@@ -3,8 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 plt.rcParams['figure.figsize']=(20,10)
 df=pd.read_csv('Bengaluru_House_Data.csv')
-print(df.head(),df.shape)
-print(df.groupby('area_type')['area_type'].agg('count'))
+
+df.groupby('area_type')['area_type'].agg('count')
 df1=df.drop(['area_type','society','availability','balcony'],axis='columns')
-"""print(df1.head())"""
-print(df1.isnull().sum())
+df2=df1.dropna()
+df3=df2.isnull().sum()
+
+
+
+
+
+
--- tempCodeRunnerFile.py
+++
@@ -1,8 +0,0 @@
-import math
-
-# Calculate the number of combinations
-n = 45
-k = 22
-combinations = math.comb(n, k)
-
-print(f"The number of distinct groups of 22 people that can be promoted is: {combinations}")
